39c7c1f4ec0a0d400c2e7bc9473938ec7827c969
2kuba1 <jakub.wojtyna06@gmail.com>
2023-10-01 16:19:28 +0200

    feat: add persistence layer reference and register services

--- ScriptShoes.API/Program.cs
+++ ScriptShoes.API/Program.cs
@@ -1,10 +1,12 @@
 using ScriptShoes.Application;
+using ScriptShoes.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddApplicationServices();
+builder.Services.AddPersistenceServices(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
