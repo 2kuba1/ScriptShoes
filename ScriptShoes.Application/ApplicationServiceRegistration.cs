314205dd6e6ff18b46fccb9d317d21f8922e63a4
2kuba1 <jakub.wojtyna06@gmail.com>
2023-10-01 16:06:33 +0200

    feat: add application service and register MediatR

---
+++ ScriptShoes.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ScriptShoes.Application;
+
+public static class ApplicationServiceRegistration
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+        
+        return services;
+    }
+}
