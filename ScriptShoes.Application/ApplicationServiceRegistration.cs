d266d0391abf8a31982453ffdeecad7144c78d76
2kuba1 <jakub.wojtyna06@gmail.com>
2023-10-04 20:55:59 +0200

    feat: add create shoe validator and add custom validation behavior

--- ScriptShoes.Application/ApplicationServiceRegistration.cs
+++ ScriptShoes.Application/ApplicationServiceRegistration.cs
@@ -1,5 +1,10 @@
 ﻿using System.Reflection;
+using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using ScriptShoes.Application.Features.Shoe.Commands.CreateShoe;
+using ScriptShoes.Application.PipelineBehaviors;
+using ScriptShoes.Application.Validators.ShoesValidator;
 
 namespace ScriptShoes.Application;
 
@@ -8,6 +13,8 @@ public static class ApplicationServiceRegistration
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         
         return services;
     }
---
+++ ScriptShoes.Application/Validators/ShoesValidator/CreateShoeValidator.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using ScriptShoes.Application.Contracts.Persistence;
+using ScriptShoes.Application.Features.Shoe.Commands.CreateShoe;
+
+namespace ScriptShoes.Application.Validators.ShoesValidator;
+
+public class CreateShoeValidator : AbstractValidator<CreateShoeCommand>
+{
+    public CreateShoeValidator(IShoeRepository repository)
+    {
+        RuleFor(x => x.Dto.Brand)
+            .NotNull();
+
+        RuleFor(x => x.Dto.Images.Count)
+            .LessThan(6)
+            .GreaterThan(0);
+
+        RuleFor(x => x.Dto.ThumbnailImage.Length)
+            .LessThan(2);
+
+        RuleFor(x => x.Dto.CurrentPrice)
+            .GreaterThan(0);
+
+        RuleFor(x => x.Dto.ShoeName)
+            .NotNull()
+            .CustomAsync(async (value, context, token) =>
+            {
+                var shoe = await repository.GetByNameAsync(value);
+                
+                if(shoe.ShoeName == value)
+                    context.AddFailure("Shoe with this name already exists");
+            });
+    }
+}
