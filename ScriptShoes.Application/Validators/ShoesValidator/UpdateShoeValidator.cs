5a6de37960d6e2210e8526aa2391811d010f5007
2kuba1 <jakub.wojtyna06@gmail.com>
2023-10-07 20:05:52 +0200

    feat: add update shoe validator

---
+++ ScriptShoes.Application/Validators/ShoesValidator/UpdateShoeValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using ScriptShoes.Application.Contracts.Persistence;
+using ScriptShoes.Application.Features.Shoe.Commands.UpdateShoe;
+
+namespace ScriptShoes.Application.Validators.ShoesValidator;
+
+public class UpdateShoeValidator : AbstractValidator<UpdateShoeCommand>
+{
+    public UpdateShoeValidator(IShoeRepository repository)
+    {
+        RuleFor(x => x.Dto.NewName)
+            .CustomAsync(async (value, context, cancellationToken) =>
+            {
+                if (value is null)
+                    return;
+
+                var shoe = await repository.GetByNameAsync(value);
+                if (shoe.ShoeName == value)
+                    context.AddFailure("This shoe already exist");
+            });
+
+        RuleFor(x => x.Dto.CurrentPrice)
+            .GreaterThan(0);
+    }
+}
