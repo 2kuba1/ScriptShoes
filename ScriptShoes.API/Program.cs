a5478730f69804163c5efa2b387c693a35fa0b5b
2kuba1 <jakub.wojtyna06@gmail.com>
2023-10-07 13:40:14 +0200

    feat: add nlog logger

--- ScriptShoes.API/Program.cs
+++ ScriptShoes.API/Program.cs
@@ -1,47 +1,68 @@
+using NLog;
+using NLog.Web;
 using ScriptShoes.Application;
 using ScriptShoes.Persistence;
 
-var builder = WebApplication.CreateBuilder(args);
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("init main");
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddApplicationServices();
-builder.Services.AddPersistenceServices(builder.Configuration);
+    builder.Services.AddApplicationServices();
+    builder.Services.AddPersistenceServices(builder.Configuration);
 
-builder.Services.AddControllers();
+    builder.Services.AddControllers();
+    
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+    
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
-builder.Services.AddResponseCaching();
+    builder.Services.AddResponseCaching();
 
-builder.Services.AddCors(cfg =>
-{
-    cfg.AddPolicy("ui", policy =>
+    builder.Services.AddCors(cfg =>
     {
-        policy.AllowAnyHeader()
-            .AllowAnyOrigin()
-            .AllowAnyMethod();
+        cfg.AddPolicy("ui", policy =>
+        {
+            policy.AllowAnyHeader()
+                .AllowAnyOrigin()
+                .AllowAnyMethod();
+        });
     });
-});
 
-var app = builder.Build();
+    var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseCors("ui");
+
+    app.UseResponseCaching();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseCors("ui");
-
-app.UseResponseCaching();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception exception)
+{
+    logger.Error(exception, "Stopped program because of exception");
+    throw;
+}
+finally
+{
+    NLog.LogManager.Shutdown();
+}
