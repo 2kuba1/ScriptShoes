9dfad9dee79d07cc6e804869fded35390f5c9f79
2kuba1 <jakub.wojtyna06@gmail.com>
2023-10-08 15:41:10 +0200

    feat: add bad request exception

---
+++ ScriptShoes.Domain/Exceptions/BadRequestException.cs
@@ -0,0 +1,9 @@
+﻿namespace ScriptShoes.Domain.Exceptions;
+
+public class BadRequestException : Exception
+{
+    public BadRequestException(string message) : base(message)
+    {
+        
+    }
+}
