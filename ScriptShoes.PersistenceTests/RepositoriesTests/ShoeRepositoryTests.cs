10824aa776b4a0ccb90aba9a64723cda15da7db0
2kuba1 <jakub.wojtyna06@gmail.com>
2023-10-10 17:50:48 +0200

    test: add shoe repository test

---
+++ ScriptShoes.PersistenceTests/RepositoriesTests/ShoeRepositoryTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ScriptShoes.Application.Contracts.Persistence;
+using ScriptShoes.Domain.Entities;
+using ScriptShoes.Persistence.Database;
+using ScriptShoes.Persistence.Repositories;
+using Shouldly;
+
+namespace ScriptShoes.PersistenceTests.RepositoriesTests;
+
+public class ShoeRepositoryTests
+{
+    private readonly IShoeRepository _shoeRepository;
+    private readonly AppDbContext _context;
+
+    public ShoeRepositoryTests()
+    {
+        var dbOptions = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+        _context = new AppDbContext(dbOptions, true);
+
+        DatabaseSeeder.SeedDatabase(_context);
+
+        _shoeRepository = new ShoeRepository(new AppDbContext(dbOptions, true));
+    }
+
+    [Fact]
+    public async Task Get_By_Name()
+    {
+        var shoe = await _shoeRepository.GetByNameAsync("Chron");
+
+        shoe.ShouldBeOfType<Shoe>();
+        shoe.ShouldNotBeNull();
+    }
+}
