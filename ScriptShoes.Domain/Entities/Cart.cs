aa474ec39c3348698b53cdbf6478d14ee2d40bcf
2kuba1 <jakub.wojtyna06@gmail.com>
2023-10-15 15:04:18 +0200

    feat: update cart

--- ScriptShoes.Domain/Entities/Cart.cs
+++ ScriptShoes.Domain/Entities/Cart.cs
@@ -5,5 +5,6 @@ namespace ScriptShoes.Domain.Entities;
 public class Cart : BaseEntity
 {
     public int UserId { get; set; }
-    public List<int> ShoeId { get; set; }
+    public int ShoeId { get; set; }
+    public int ItemCount { get; set; }
 }
