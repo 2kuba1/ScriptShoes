3788d32de51eb4717ea2245e2238ceaee2a6a3a6
2kuba1 <jakub.wojtyna06@gmail.com>
2023-10-17 18:09:50 +0200

    feat: add AddToCart validator

---
+++ ScriptShoes.Application/Validators/CartValidator/AddToCartValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using ScriptShoes.Application.Features.Cart.Commands.AddToCart;
+
+namespace ScriptShoes.Application.Validators.CartValidator;
+
+public class AddToCartValidator : AbstractValidator<AddToCartCommand>
+{
+    public AddToCartValidator()
+    {
+        RuleFor(x => x.ItemsCount)
+            .GreaterThan(0);
+    }
+}
