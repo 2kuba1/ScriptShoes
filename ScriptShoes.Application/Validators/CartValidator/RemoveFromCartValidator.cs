1b2d7ad11f0b73ab47fd28230428f36a1576da63
2kuba1 <jakub.wojtyna06@gmail.com>
2023-10-18 20:00:43 +0200

    feat: add remove from cart validator

---
+++ ScriptShoes.Application/Validators/CartValidator/RemoveFromCartValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using ScriptShoes.Application.Features.Cart.Commands.RemoveFromCart;
+
+namespace ScriptShoes.Application.Validators.CartValidator;
+ 
+public class RemoveFromCartValidator : AbstractValidator<RemoveFromCartCommand>
+{
+    public RemoveFromCartValidator()
+    {
+        RuleFor(x => x.ItemsCount)
+            .GreaterThan(0);
+    }
+}
