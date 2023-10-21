36924796ff32f3484d71d52f6062f0f9c96be1d1
2kuba1 <jakub.wojtyna06@gmail.com>
2023-10-21 13:17:24 +0200

    feat: add create review validator

---
+++ ScriptShoes.Application/Validators/ReviewValidator/CreateReviewValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using ScriptShoes.Application.Features.Review.Commands;
+
+namespace ScriptShoes.Application.Validators.ReviewValidator;
+
+public class CreateReviewValidator : AbstractValidator<CreateReviewCommand>
+{
+    public CreateReviewValidator()
+    {
+        RuleFor(x => x.Dto.Title)
+            .MinimumLength(3)
+            .MaximumLength(50);
+
+        RuleFor(x => x.Dto.ReviewDescription)
+            .MinimumLength(5)
+            .MaximumLength(500);
+
+        RuleFor(x => x.Dto.ShoeRate)
+            .GreaterThanOrEqualTo(1)
+            .LessThanOrEqualTo(5);
+    }
+}
