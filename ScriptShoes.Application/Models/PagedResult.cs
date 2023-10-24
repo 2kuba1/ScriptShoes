0dc0e18e5b8053f689cd2b5d48e99660b5694985
2kuba1 <jakub.wojtyna06@gmail.com>
2023-10-24 20:21:45 +0200

    feat: add paged result model

---
+++ ScriptShoes.Application/Models/PagedResult.cs
@@ -0,0 +1,19 @@
+﻿namespace ScriptShoes.Application.Models;
+
+public class PagedResult<T>
+{
+    public List<T> Item { get; set; }
+    public int TotalPages { get; set; }
+    public int ItemsFrom { get; set; }
+    public int ItemsTo { get; set; }
+    public int TotalItemsCount { get; set; }
+
+    public PagedResult(List<T> item, int totalItemsCount, int pageSize, int pageNumber)
+    {
+        Item = item;
+        TotalItemsCount = totalItemsCount;
+        ItemsFrom = pageSize * (pageNumber - 1) + 1;
+        ItemsTo = ItemsFrom + pageSize - 1;
+        TotalPages = (int)Math.Ceiling(totalItemsCount / (double)pageSize);
+    }
+}
