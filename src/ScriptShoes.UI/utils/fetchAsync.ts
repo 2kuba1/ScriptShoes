23931ee869f378ae76929d081f1f8816c234667f
2kuba1 <jakub.wojtyna06@gmail.com>
2023-12-10 22:57:42 +0100

    fix: post requests feat: add refetch

--- src/ScriptShoes.UI/utils/fetchAsync.ts
+++ src/ScriptShoes.UI/utils/fetchAsync.ts
@@ -1,8 +1,14 @@
-import axios, { AxiosError } from 'axios';
+import axios, { AxiosError, AxiosRequestConfig } from 'axios';
 
 interface FetchAsync<T> {
   error: AxiosError | null;
   data: T;
+  refetch: (newHeaders: any) => Promise<RefetchAsync<T>>;
+}
+
+interface RefetchAsync<T> {
+  refetchError: AxiosError | null;
+  refetchData: T;
 }
 
 export enum Method {
@@ -21,18 +27,38 @@ const fetchAsync = async <T>(
 ): Promise<FetchAsync<T>> => {
   let fetchData = {} as FetchAsync<T>;
 
+  const config = {
+    url: url,
+    method: method,
+    data: body ? body : null,
+    headers: headers ? headers : null,
+  } as AxiosRequestConfig;
+
   try {
-    const { data }: { data: T } = await axios(url, {
-      method: method,
-      params: body ? body : null,
-      headers: headers ? headers : null,
-    });
+    const { data }: { data: T } = await axios(config);
     fetchData.data = data;
   } catch (err) {
     if (err instanceof AxiosError) {
       fetchData.error = err;
     }
   }
+
+  fetchData.refetch = async <T>(newHeaders: any | null) => {
+    let refetchData = {} as RefetchAsync<T>;
+
+    try {
+      config.headers = newHeaders;
+
+      const { data }: { data: T } = await axios(config);
+      refetchData.refetchData = data;
+    } catch (err) {
+      if (err instanceof AxiosError) {
+        refetchData.refetchError = err;
+      }
+    }
+
+    return refetchData;
+  };
 
   return fetchData;
 };
