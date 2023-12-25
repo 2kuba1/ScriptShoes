2539e37e917e59b560eb735fed88994d2b1375d6
2kuba1 <jakub.wojtyna06@gmail.com>
2023-12-25 12:14:04 +0100

    test: remove infrastructure tests, rewrite CreateCommandHandlerTests

--- src/ScriptShoes.Application/Features/Shoe/Commands/CreateShoe/CreateShoeCommandHandler.cs
+++ src/ScriptShoes.Application/Features/Shoe/Commands/CreateShoe/CreateShoeCommandHandler.cs
@@ -1,6 +1,7 @@
 ﻿using Mapster;
 using MediatR;
 using ScriptShoes.Application.Contracts.Persistence;
+using ScriptShoes.Application.Exceptions;
 
 namespace ScriptShoes.Application.Features.Shoe.Commands.CreateShoe;
 
@@ -14,12 +15,15 @@ public class CreateShoeCommandHandler : IRequestHandler<CreateShoeCommand, int>
         _repository = repository;
         _userRepository = userRepository;
     }
-    
+
     public async Task<int> Handle(CreateShoeCommand request, CancellationToken cancellationToken)
     {
         var shoe = request.Dto.Adapt<Domain.Entities.Shoe>();
 
-        if (_userRepository.GetUserId != null) shoe.UserId = _userRepository.GetUserId.Value;
+        if (_userRepository.GetUserId is null)
+            throw new NotFoundException($"User with id '{_userRepository.GetUserId}' not found");
+
+        shoe.UserId = _userRepository.GetUserId.Value;
 
         await _repository.CreateAsync(shoe);
 
---
+++ tests/ScriptShoes.Application.Tests/Shoe/Commands/CreateShoeCommandHandlerTests.cs
@@ -0,0 +1,71 @@
+﻿using FluentAssertions;
+using Moq;
+using ScriptShoes.Application.Contracts.Persistence;
+using ScriptShoes.Application.Exceptions;
+using ScriptShoes.Application.Features.Shoe.Commands.CreateShoe;
+using ScriptShoes.Application.Models.Shoe;
+
+namespace ScriptShoes.Application.Tests.Shoe.Commands;
+
+public class CreateShoeCommandHandlerTests
+{
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    public async Task Handle_CreatesShoe_ForGivenUserId(int userId)
+    {
+        //arrange 
+
+        var command = new CreateShoeCommand(new CreateShoeDto()
+        {
+            ShoeName = "Test",
+            Quantity = 50,
+            CurrentPrice = 99.99f,
+        });
+
+        var userRepositoryMock = new Mock<IUserRepository>();
+
+        userRepositoryMock.Setup(c => c.GetUserId).Returns(userId);
+
+        var shoeRepositoryMock = new Mock<IShoeRepository>();
+
+        var handler = new CreateShoeCommandHandler(shoeRepositoryMock.Object, userRepositoryMock.Object);
+
+        //act
+
+        var result = await handler.Handle(command, CancellationToken.None);
+
+        //assert
+
+        result.Should().Be(0);
+    }
+
+    [Fact]
+    public async Task Handle_ThrowsNotFoundException_ForNullUserId()
+    {
+        //arrange 
+
+        var command = new CreateShoeCommand(new CreateShoeDto()
+        {
+            ShoeName = "Test",
+            Quantity = 50,
+            CurrentPrice = 99.99f,
+        });
+
+        var userRepositoryMock = new Mock<IUserRepository>();
+
+        userRepositoryMock.Setup(c => c.GetUserId).Returns((int?)null);
+
+        var shoeRepositoryMock = new Mock<IShoeRepository>();
+
+        var handler = new CreateShoeCommandHandler(shoeRepositoryMock.Object, userRepositoryMock.Object);
+
+        //assert
+
+        Func<Task> action = async () => await handler.Handle(command, CancellationToken.None);
+
+        //act
+
+        await Assert.ThrowsAsync<NotFoundException>(action);
+    }
+}
