aa6088b4f20a924b025fa1614699668947517d8d
2kuba1 <jakub.wojtyna06@gmail.com>
2024-01-01 11:04:38 +0100

    test: add RemoveDiscountCommandHandler tests

---
+++ tests/ScriptShoes.Application.Tests/Discount/Commands/RemoveDiscountCommandHandlerTests.cs
@@ -0,0 +1,66 @@
+﻿using FluentAssertions;
+using MediatR;
+using Moq;
+using ScriptShoes.Application.Contracts.Persistence;
+using ScriptShoes.Application.Exceptions;
+using ScriptShoes.Application.Features.Discount.Commands.RemoveDiscount;
+
+namespace ScriptShoes.Application.Tests.Discount.Commands;
+
+public class RemoveDiscountCommandHandlerTests
+{
+    [Fact]
+    public async Task Handle_ForGivenDiscountId_RemovesDiscount()
+    {
+        //arrange
+
+        var command = new RemoveDiscountCommand(1);
+
+        var discountRepository = new Mock<IDiscountRepository>();
+
+        var discount = new Domain.Entities.Discount()
+        {
+            Id = 1
+        };
+
+        discountRepository.Setup(d => d.GetByIdAsync(1)).ReturnsAsync(discount);
+        discountRepository.Setup(d => d.DeleteDiscount(discount));
+
+        var handler = new RemoveDiscountCommandHandler(discountRepository.Object);
+
+        //act
+
+        var result = await handler.Handle(command, CancellationToken.None);
+
+        //assert
+
+        result.Should().Be(Unit.Value);
+    }
+
+    [Fact]
+    public async Task Handle_ForNullDiscountObject_ThrowsNotFoundException()
+    {
+        //arrange
+
+        var command = new RemoveDiscountCommand(1);
+
+        var discountRepository = new Mock<IDiscountRepository>();
+
+        var discount = new Domain.Entities.Discount()
+        {
+            Id = 1
+        };
+
+        discountRepository.Setup(d => d.GetByIdAsync(1)).ReturnsAsync((Domain.Entities.Discount)null);
+
+        var handler = new RemoveDiscountCommandHandler(discountRepository.Object);
+
+        //act
+
+        Func<Task> action = async () => await handler.Handle(command, CancellationToken.None);
+
+        //assert
+
+        await action.Should().ThrowAsync<NotFoundException>();
+    }
+}
