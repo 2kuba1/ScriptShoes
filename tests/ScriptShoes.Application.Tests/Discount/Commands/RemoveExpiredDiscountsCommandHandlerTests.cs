f4094850fe1cdd53581df1cc998289a777fec9dd
2kuba1 <jakub.wojtyna06@gmail.com>
2024-01-01 11:22:00 +0100

    test: add RemoveExpiredDiscountsCommandHandler tests

---
+++ tests/ScriptShoes.Application.Tests/Discount/Commands/RemoveExpiredDiscountsCommandHandlerTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using MediatR;
+using Moq;
+using ScriptShoes.Application.Contracts.Persistence;
+using ScriptShoes.Application.Features.Discount.Commands.RemoveExpiredDiscounts;
+
+namespace ScriptShoes.Application.Tests.Discount.Commands;
+
+public class RemoveExpiredDiscountsCommandHandlerTests
+{
+    [Fact]
+    public async Task Handle_ForGivenDiscounts_RemovesExpiredDiscounts()
+    {
+        //arrange
+
+        var discounts = new List<Domain.Entities.Discount>()
+        {
+            new Domain.Entities.Discount()
+            {
+                Id = 1,
+                DiscountEndDateTime = new DateTime(2023, 12, 30)
+            },
+            new Domain.Entities.Discount()
+            {
+                Id = 2,
+                DiscountEndDateTime = new DateTime(2024, 12, 30)
+            },
+        };
+
+        var discountRepository = new Mock<IDiscountRepository>();
+
+        discountRepository.Setup(d => d.RemoveExpiredDiscounts(discounts));
+
+        var handler = new RemoveExpiredDiscountsCommandHandler(discountRepository.Object);
+
+        //act
+
+        var result = await handler.Handle(new RemoveExpiredDiscountsCommand(discounts), CancellationToken.None);
+
+        //assert
+
+        result.Should().Be(Unit.Value);
+    }
+}
