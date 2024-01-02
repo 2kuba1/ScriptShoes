2afd46a609c3683a1ca45e346465fb1150b45e8f
2kuba1 <jakub.wojtyna06@gmail.com>
2024-01-02 19:13:37 +0100

    test: GetExpiredDiscountsQueryHandler tests

---
+++ tests/ScriptShoes.Application.Tests/Discount/Queries/GetExpiredDiscountsQueryHandlerTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using Moq;
+using ScriptShoes.Application.Contracts.Persistence;
+using ScriptShoes.Application.Features.Discount.Queries.GetExpiredDiscounts;
+
+namespace ScriptShoes.Application.Tests.Discount.Queries;
+
+public class GetExpiredDiscountsQueryHandlerTests
+{
+    [Fact]
+    public async Task Handle_ReturnsExpiredDiscounts()
+    {
+        //arrange
+
+        var discounts = new List<Domain.Entities.Discount>()
+        {
+            new Domain.Entities.Discount()
+            {
+                Id = 1,
+            },
+            new Domain.Entities.Discount()
+            {
+                Id = 2,
+            }
+        };
+
+        var discountRepository = new Mock<IDiscountRepository>();
+
+        discountRepository.Setup(d => d.GetExpiredDiscounts()).ReturnsAsync(discounts);
+
+        var handler = new GetExpiredDiscountsQueryHandler(discountRepository.Object);
+        
+        //act
+
+        var result = await handler.Handle(new GetExpiredDiscountsQuery(), CancellationToken.None);
+        
+        //assert
+
+        result.Should().BeEquivalentTo(discounts);
+    }
+}
