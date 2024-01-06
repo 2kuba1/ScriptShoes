ae0e8038c8f67c110512a2d601463bb7be3e6bb7
2kuba1 <jakub.wojtyna06@gmail.com>
2024-01-06 15:32:20 +0100

    test: add RemoveOrderCommandHandler tests

---
+++ tests/ScriptShoes.Application.Tests/Orders/Commands/RemoveOrderCommandHandlerTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using MediatR;
+using Moq;
+using ScriptShoes.Application.Contracts.Persistence;
+using ScriptShoes.Application.Features.Orders.Commands.RemoveOrder;
+
+namespace ScriptShoes.Application.Tests.Orders.Commands;
+
+public class RemoveOrderCommandHandlerTests
+{
+    [Fact]
+    public async Task Handle_ForGivenOrderSessionId_RemovesOrder()
+    {
+        //arrange
+
+        var command = new RemoveOrderCommand("test");
+
+        var orderRepository = new Mock<IOrderRepository>();
+
+        orderRepository.Setup(o => o.RemoveOrder(command.OrderSessionId));
+
+        var handler = new RemoveOrderCommandHandler(orderRepository.Object);
+        
+        //act
+
+        var result = await handler.Handle(command, CancellationToken.None);
+        
+        //assert
+
+        result.Should().Be(Unit.Value);
+    }
+}
